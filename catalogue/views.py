e264190777ef971ac6d8497906e6ae7a92b4e74c
Tonatihu Barrera <carlostonatihu@gmail.com>
2017-05-21 16:33:56 -0500

    Ya esta la funcionalidad calificar/favoritos en serie pero no se si funcione

--- catalogue/views.py
+++ catalogue/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 
 from catalogue.models import Content, Episode
 
-
 # Create your views here.
 from userprofiles.models import History
 
@@ -21,6 +20,7 @@ def cargar_info_usuario(request):
     }
     return data
 
+
 def buscar(request):
     content = None
     name = None
@@ -33,6 +33,7 @@ def buscar(request):
     if name is None:
         content = Content.objects.all()
     return content
+
 
 @login_required(login_url='/user/login/')
 def content_list(request):
@@ -58,6 +59,11 @@ def film_detail(request, pk):
 def serie_detail(request, pk):
     serie = Content.objects.get(pk=pk)
     data = cargar_info_usuario(request)
+    temp_user = request.user.userprofile
+    if History.objects.filter(user=temp_user, content=serie, is_favorite=True).exists():
+        data['fav_submit'] = 'Quitar de favoritos'
+    else:
+        data['fav_submit'] = 'Agregar a favoritos'
     return render(request, 'serie.html', {'serie': serie, 'data': data})
 
 
@@ -66,27 +72,6 @@ def episode_detail(request, pk):
     episode = Episode.objects.get(pk=pk)
     data = cargar_info_usuario(request)
     return render(request, 'episode.html', {'episode': episode, 'data': data})
-
-
-# class ContentListView(LoginRequiredMixin, ListView):
-#     model = Content
-#     template_name = 'catalogo.html'
-#     context_object_name = 'catalogue'
-#     login_url = '/user/login'
-#
-#     def get_queryset(self):
-#         if self.kwargs.get('name'):
-#             queryset = self.model.objects.filter(title__contains=self.kwargs['name'])
-#         else:
-#             queryset = super(ContentListView, self).get_queryset()
-#         return queryset
-#
-#     def get_context_data(self, **kwargs):
-#         context = super(ContentListView, self).get_context_data(**kwargs)
-#         context['busqueda'] = True
-#         data = cargar_info_usuario(self.request)
-#         context['data'] = data
-#         return context
 
 def agregar_favorito_view(request):
     data = {'error': 'error'}
@@ -105,6 +90,7 @@ def agregar_favorito_view(request):
                 history = History.objects.create(user=temp_user, content=film, is_favorite=True)
                 history.save()
     return JsonResponse(data)
+
 
 def puntuar_view(request):
     data = {'ok': False}
