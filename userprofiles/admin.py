6f97bfa0252c2efceb5be0a4b189695882ccea0e
Carlos Barrera <carlostonatihu@gmail.com>
2017-06-14 23:27:48 -0500

    Listo para presentar

--- userprofiles/admin.py
+++ userprofiles/admin.py
@@ -1,22 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 
 from userprofiles.models import UserProfile, History, CreditCard
 
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'email', 'type_of_user')
+# @admin.register(UserProfile)
+# class UserProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'email', 'type_of_user')
 
 
 @admin.register(History)
 class UserHistoryAdmin(admin.ModelAdmin):
     list_display = ('user_profile', 'content', 'is_favorite', 'score')
 
-@admin.register(CreditCard)
-class CreditCardAdmin(admin.ModelAdmin):
-    list_display = ('user_profile', 'owner', 'number', 'due_month', 'due_year')
+# @admin.register(CreditCard)
+# class CreditCardAdmin(admin.ModelAdmin):
+#     list_display = ('user_profile', 'owner', 'number', 'due_month', 'due_year')
 
 admin.site.unregister(Group)
+admin.site.unregister(User)
